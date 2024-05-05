b53b9f40d01a46f90010ce2e2fa843887b09f4af
Julianj-mtz <a01254258@tec.mx>
2024-05-04 18:21:43 -0700

    change global css and add theme provider component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,9 @@ const config = {
     },
     extend: {
       colors: {
+        turquesa: "#0798A5",
+        azulClaro: "#DEF2FB",
+        turquesaOscuro: "#06818C",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
