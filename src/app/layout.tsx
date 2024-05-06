926713ea67d38efd1fc927c49c1ded702dfaec42
Julianj-mtz <a01254258@tec.mx>
2024-05-06 12:57:42 -0700

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-        <Header />
+          <Header />
 
           {children}
         </ThemeProvider>
