0deedeecdc36b186d14f823c77c675fc781f5f19
Julianj-mtz <a01254258@tec.mx>
2024-05-13 16:43:06 -0700

    change colors to english in tailwind.congig.ts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
               <DropdownMenuTrigger asChild>
                 <Button
                   variant="outline"
-                  className="border-turquesaOscuro bg-turquesaOscuro rounded-full"
+                  className="border-darkTurquoise bg-darkTurquoise rounded-full"
                   size="icon"
                 >
                   <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
