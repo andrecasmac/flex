a36b50720d7d99213595f2aa80c83c068db61700
ElMeGaMerX <A01254039@tec.mx>
2024-05-13 18:25:38 -0700

    add: shadcn components to create Combobox component

---
+++ src/app/examples/dropdown/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { SearchDropdown } from "@/components/ui/searchDropdown";
+
+export default function test(){
+    return (
+        <main>
+            {/*<SearchDropdown />*/}
+        </main>
+    )
+}
