fbcc8a0c90bc061e6b1fe13e9f15ef9263cf20aa
Julianj-mtz <a01254258@tec.mx>
2024-05-14 18:58:03 -0700

    changed role selector screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,47 @@
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import { Card, CardFooter } from "@/components/ui/card";
 
 import { AdminIcon, ClientIcon } from "../assets/clientsIcons";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-[70%] max-md:w-[90%] transition-all ">
-        <Card className=" bg-ligthBlue dark:bg-cyan-950 h-[40em] max-md:h-[45em] flex flex-col justify-center">
-          <div>
-            <div className="flex md:justify-around max-md:flex-col max-md:items-center max-md:justify-center h-full  max-md:space-y-3">
-              <Card className=" rounded-2xl w-[70%] md:w-[35%] md:ms-[2.5em]">
-                <div className="flex flex-col justify-center items-center ">
-                  <p className="py-3  text-turquoise">Administrator</p>
-                  <AdminIcon />
-                  <div className="pb-3 pt-4">
-                    <Button
-                      asChild
-                      variant="default"
-                      size="sm"
-                      className="shadow-sm"
-                    >
-                      <a href="/admin">Select</a>
-                    </Button>
-                  </div>
+    <main className="flex flex-col justify-center items-center h-screen">
+      <div className="h-[60%] w-[80%] max-md:h-[78%]">
+        <Card className="bg-ligthBlue dark:bg-cyan-950 h-full flex flex-col items-center justify-center">
+          <div className="grid grid-cols-2 w-full justify-center gap-x-3 px-4 max-md:flex max-md:flex-col max-md:gap-y-4">
+            <div className=" flex justify-center items-center">
+              <Card className="p-3 flex flex-col justify-center items-center md:w-[65%] max-md:w-[80%] md:h-[90%] md:ms-[3em]">
+                <p className="pb-3 text-turquoise">Admin</p>
+                <AdminIcon />
+
+                <div className="pt-4">
+                  <Button
+                    asChild
+                    variant="default"
+                    size="sm"
+                    className="shadow-sm"
+                  >
+                    <a href="/admin">Select</a>
+                  </Button>
                 </div>
               </Card>
+            </div>
 
-              <Card className="rounded-2xl w-[70%] md:w-[35%] md:me-[2.5em]">
-                <div className="flex flex-col justify-center items-center ">
-                  <p className="py-3 text-turquoise">Client</p>
-                  <ClientIcon />
-                  <div className="pb-3 pt-4">
-                    <Button
-                      asChild
-                      variant="default"
-                      size="sm"
-                      className="shadow-sm"
-                    >
-                      <a href="/client">Select</a>
-                    </Button>
-                  </div>
+            <div className=" flex justify-center items-center">
+              <Card className="p-3 flex flex-col justify-center items-center md:w-[65%] max-md:w-[80%] md:h-[90%] md:me-[3em]">
+                <p className="pb-3 text-turquoise">Client</p>
+
+                <ClientIcon />
+
+                <div className="pt-4">
+                  <Button
+                    asChild
+                    variant="default"
+                    size="sm"
+                    className="shadow-sm"
+                  >
+                    <a href="/client">Select</a>
+                  </Button>
                 </div>
               </Card>
             </div>
@@ -48,11 +49,11 @@ export default function Home() {
         </Card>
       </div>
 
-      {/* <div className="pt-1">
+      <div className="pt-3">
         <Button asChild variant="default" size="sm" className="shadow-sm">
           <a href="/examples">Components</a>
         </Button>
-      </div> */}
+      </div>
     </main>
   );
 }
