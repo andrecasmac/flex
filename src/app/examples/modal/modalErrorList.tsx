9aa9d596c2df69b4ca1ff4d961bb9d6e51c17ff3
ElMeGaMerX <A01254039@tec.mx>
2024-05-20 10:58:22 -0700

    add: DataTable with a GET function

--- src/app/examples/modal/modalErrorList.tsx
+++ src/app/examples/modal/modalErrorList.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useState,useEffect } from "react";
 import {
   DialogContent,
   DialogHeader,
@@ -11,8 +11,12 @@ import {
 } from "@/components/ui/dialog";
 
 import { Button } from "@/components/ui/button";
+import { DataTable } from "../tables/table/data-table";
+import { columnsErrorList, ErrorList } from "../tables/table/colums";
+import { getTable, getTableError } from "@/hooks/dataTable";
 
-interface ModalTestProps {
+
+interface ModalErrorProps {
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
   ButtonContent: string;
@@ -22,29 +26,39 @@ export function ModalErrorList({
   isOpen,
   setIsOpen,
   ButtonContent,
-}: ModalTestProps) {
+}: ModalErrorProps) {
+  const [data, setData]= useState<ErrorList[]>([]);
+  useEffect(()=>{
+    const fetchErrorData = async () => {
+      try {
+        const result:ErrorList[] = await getTableError();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchErrorData();
+  }, []);
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="default">{ButtonContent} </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[625px]">
-        <div className="flex items-center justify-center pt-10"> 
-        ... 
+        <div className="flex items-center justify-center pt-10">
+          <DataTable columns={columnsErrorList} data={data} />
         </div>
-
-        <DialogFooter className="felx items-center justify-center">
+        <DialogFooter className="flex items-center justify-center">
           <DialogClose asChild>
             <Button size="sm" className="h-8 w-[40%]">
               Cancel
             </Button>
           </DialogClose>
           <Button size="sm" className="h-8 w-[40%]">
-              Try Again
-            </Button>
-          {/* <Button size="sm" type="submit" className="h-8 w-[40%]">
-            Save
-          </Button> */}
+            Try Again
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
