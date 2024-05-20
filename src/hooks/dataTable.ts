e361ad91e6bd477d31cf6da99fc85773cfbf3a72
ElMeGaMerX <A01254039@tec.mx>
2024-05-20 10:53:17 -0700

    add: async GET function for the ErrorTable

--- src/hooks/dataTable.ts
+++ src/hooks/dataTable.ts
@@ -1,6 +1,7 @@
 
-import { ProductsT } from "@/app/examples/tables/table/colums";
+import { ProductsT, ErrorList } from "@/app/examples/tables/table/colums";
 import { fetchAllProducts } from "@/app/apis/products/route";
+import { fetchAllErrors } from "@/app/apis/errors/route";
 
 export async function getTable(): Promise<ProductsT[]> {
     try {
@@ -9,6 +10,17 @@ export async function getTable(): Promise<ProductsT[]> {
         return dataProducts;
     } catch (error) {
         console.error("Error fetching products:", error);
+        throw error;
+    }
+}
+
+export async function getTableError(): Promise<ErrorList[]> {
+    try {
+        const errorsData = await fetchAllErrors();
+        const dataErrors: ErrorList[] = await errorsData.json();
+        return dataErrors;
+    } catch (error) {
+        console.error("Error fetching errors:", error);
         throw error;
     }
 }
