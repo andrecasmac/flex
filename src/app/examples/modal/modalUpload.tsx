d621a3d7ac66c03557dba85318907a800eb9b587
Kmaldon <A01252830@itesm.mx>
2024-05-21 15:53:20 -0700

    Added Comments and a error message for Validate Button

--- src/app/examples/modal/modalUpload.tsx
+++ src/app/examples/modal/modalUpload.tsx
@@ -28,28 +28,55 @@ export function ModalUpload ({
     setIsOpen,
     ButtonContent,
 }:ModalUploadProps) {
+
+    // State to store the selected file
     const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+
+    // State to store the content of the selected .txt file
     const [txtFileContent, setTxtFileContent] = useState<string | null>(null);
 
+    // State to send error when pressing the validate button with no file in the Dropzone
+    const [errorValidate, setErrorValidate] = useState<string | null>(null);
+
+    // Callback function to handle file drop event
     const onDrop = useCallback((acceptedFiles: File[]) => {
+
+        // Only process the first file, assuming single file upload
         const file = acceptedFiles[0];
         setUploadedFile(file);
         console.log("Uploaded File: ", file)
 
+        // For reading the .txt file content
         const textReader = new FileReader();
         textReader.onload = () => {
+            
+            // Store the file content in the state once reading is complete
             const content = textReader.result as string;
             setTxtFileContent(content);
         };
+
+        // Will read the file as string
         textReader.readAsText(file);
+
+        // Clear error if file selected
+        setErrorValidate(null)
     }, []);
 
+    // Create a get root and input props from useDropzone
     const { getRootProps, getInputProps} = useDropzone({ onDrop, maxFiles: 1 });
 
-    const handleSubmit = () => {
+    // Function to handle the Valitate Button
+    const handleValidate = () => {
+
+        // If Dropzone doesn't have a file
         if (!uploadedFile) {
-            console.log("No File Validated");
+            // An error text will appear when no file is found
+            setErrorValidate("*Please upload a file before validating");
+            console.log("No File to Validated");
+            return;
         }
+
+        // Will validate the file and close the Dropzone
         else{
             console.log("Validated File: ", uploadedFile);
             setIsOpen(false);
@@ -57,7 +84,10 @@ export function ModalUpload ({
     };
 
     return (
+        // Render the Modal for Uploading Documents with Dropzone
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
+
+            {/* Button to open Modal */}
             <DialogTrigger asChild>
                 <Button variant="default">{ButtonContent} </Button>
             </DialogTrigger>
@@ -65,25 +95,36 @@ export function ModalUpload ({
             <DialogPortal>
                 <DialogContent className="sm:max-w-[800px] p-10">
 
+                    {/* Dropzone area */}
                     <div className="flex items-center justify-center p-2 h-72">
-                        <div {...getRootProps()} className="bg-ligthBlue rounded-md h-full w-full cursor-pointer text-center flex flex-col items-center justify-center">
+                        <div {...getRootProps()} className="bg-ligthBlue rounded-md h-full w-full cursor-pointer text-center flex flex-col items-center justify-center" >
+                            {/* Input element for file selection */}
                             <input {...getInputProps()} />
                             <UploadIcon color="Gray" width="80" height="80"/>
                             <p className="text-gray-500 mt-2">Upload your document</p>
+
+                            {/* The name of the File selected */}
                             {uploadedFile && <p className="text-gray-500 underline pt-4">{uploadedFile.name}</p>}
-                            {txtFileContent && <p className="text-gray-500 pt-4">{txtFileContent}</p>}
+
+                            {/* If .txt, it will show the contents of the file */}
+                            {txtFileContent && <pre className="text-gray-500 pt-4 text-left">{txtFileContent}</pre>}
                         </div>
                     </div>
 
+                    {/* Error message */}
+                    {errorValidate && <p className="text-red-700 ms-9">{errorValidate}</p>}
+
                     <div className="flex justify-around w-full">
 
+                        {/* Button to close the Modal */}
                         <DialogClose asChild>
                             <Button size="sm" type="button" className="h-8 w-[40%]">
                                 Cancel
                             </Button>
                         </DialogClose>
 
-                        <Button size="sm" type="submit" onClick={handleSubmit} className="h-8 w-[40%]">
+                        {/* Validate Button */}
+                        <Button size="sm" type="submit" onClick={handleValidate} className="h-8 w-[40%]">
                             Validate
                         </Button>
 
