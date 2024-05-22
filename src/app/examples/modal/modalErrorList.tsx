342caa52f148c7ccb71cc8fce7cc89acd95cb1d3
andrecasmac <a01254180@tec.mx>
2024-05-21 22:59:22 -0700

    Added comment to errors modal

--- src/app/examples/modal/modalErrorList.tsx
+++ src/app/examples/modal/modalErrorList.tsx
@@ -14,6 +14,7 @@ import { Button } from "@/components/ui/button";
 import { DataTable } from "../tables/table/data-table";
 import { columnsErrorList, ErrorList } from "../tables/table/colums";
 
+// Importing errors from json
 import errors from "./modalErrorData.json"
 
 interface ModalErrorProps {
