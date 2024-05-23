c0efa44bf05635d9ce385c99b0c3aa526f8394de
ElMeGaMerX <A01254039@tec.mx>
2024-05-23 16:26:26 -0700

    fix: type of error context provider and naming

--- src/app/context/errorContextProvider.tsx
+++ src/app/context/errorContextProvider.tsx
@@ -3,10 +3,10 @@
 'use client';
 
 import ErrorContext from './errorContext';
-import React, { ReactElement, useState } from 'react';
+import React, { PropsWithChildren, ReactElement, useState } from 'react';
 import { ErrorList } from '../examples/tables/table/colums';
 
-const MyContextProvider: React.FC = ({ children }:any) => {
+const ErrorContextProvider: React.FC<PropsWithChildren> = ({ children }:any) => {
     const [errorlistShareData, setErrorListShareData] = useState<ErrorList[]>([]);
 
   return (
@@ -16,4 +16,4 @@ const MyContextProvider: React.FC = ({ children }:any) => {
   );
 };
 
-export default MyContextProvider;
+export default ErrorContextProvider;
