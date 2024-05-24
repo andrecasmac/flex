877cdb002b91c4d65b0e63df44905521fdcc0d07
Andre Castillo <a01254180@itesm.mx>
2024-05-23 17:14:50 -0700

    Changed data access button text color

--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -67,7 +67,7 @@ export default function Page() {
         asChild
         variant="default"
         size="lg"
-        className="rounded-2xl text-white active:scale-95 transition-all"
+        className="rounded-2xl active:scale-95 transition-all"
       >
         <a href="/examples/DA">Data Access</a>
       </Button>
