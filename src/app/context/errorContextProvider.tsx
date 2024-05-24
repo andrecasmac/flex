d6bd2ae043164bba23720e30bf25a01678e6a9fa
ElMeGaMerX <A01254039@tec.mx>
2024-05-23 18:51:20 -0700

    edit: added  is open modal parameters to context

--- src/app/context/errorContextProvider.tsx
+++ src/app/context/errorContextProvider.tsx
@@ -8,9 +8,10 @@ import { ErrorList } from '../examples/tables/table/colums';
 
 const ErrorContextProvider: React.FC<PropsWithChildren> = ({ children }:any) => {
     const [errorlistShareData, setErrorListShareData] = useState<ErrorList[]>([]);
+    const [isOtherOpen, setOtherIsOpen] = useState(false);
 
   return (
-    <ErrorContext.Provider value={{ errorlistShareData, setErrorListShareData}}>
+    <ErrorContext.Provider value={{ errorlistShareData, setErrorListShareData,isOtherOpen,setOtherIsOpen}}>
       {children}
     </ErrorContext.Provider>
   );
