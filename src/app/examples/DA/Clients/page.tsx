ab4b625023644e9e9ba08687eabc3b2cff6d93e9
Said-29 <a01252477@tec.mx>
2024-05-27 16:44:54 -0700

    -Added seed and seeder -Implemented controllers to partner's list (admin) -Changed schema to fit properly with the project needs

--- src/app/examples/DA/Clients/page.tsx
+++ src/app/examples/DA/Clients/page.tsx
@@ -1,28 +1,7 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import { getAllClients } from '../../../da/Clients/client-da'; 
-
-//Types should go in a separate file, but for the sake of the example, they are defined here
-type Partner = {
-  id: string;
-  name: string;
-  edi_version: string;
-  delimiters: string;
-  EOL: string;
-  type_of_connection: string;
-  PO_Test: any;
-};
-
-type AssociatedPartner = {
-  id: string;
-  partner: Partner | null;
-};
-
-type Client = {
-  id: string;
-  name: string;
-  partnerships: AssociatedPartner | null;
-};
+import { getAllClients } from '../../../../da/Clients/client-da'; 
+import { Client } from '../../../../../types/DbTypes';
 
 const ClientsList: React.FC = () => {
   const [clients, setClients] = useState<Client[]>([]);
@@ -59,32 +38,36 @@ const ClientsList: React.FC = () => {
         clients.map(client => (
           <div key={client.id} className='border border-gray-300 p-5 mb-10'>
             <h2>{client.name}</h2>
-            {client.partnerships ? (
-              <div>
-                <h3>Partnership</h3>
-                <p>ID: {client.partnerships.id}</p>
-                {client.partnerships.partner ? (
-                  <div>
-                    <h4>Partner Details</h4>
-                    <p>Name: {client.partnerships.partner.name}</p>
-                    <p>EDI Version: {client.partnerships.partner.edi_version}</p>
-                    <p>Delimiters: {client.partnerships.partner.delimiters}</p>
-                    <p>EOL: {client.partnerships.partner.EOL}</p>
-                    <p>Type of Connection: {client.partnerships.partner.type_of_connection}</p>
-                    <pre>PO Test: {JSON.stringify(client.partnerships.partner.PO_Test, null, 2)}</pre>
-                  </div>
-                ) : (
-                  <p>No partner details available</p>
-                )}
-              </div>
-            ) : (
+            {client.partnerships.length > 0 ? (
+              client.partnerships.map(partnership => (
+                <div key={partnership.id}>
+                  <h3>Partnership</h3>
+                  <p>ID: {partnership.id}</p>
+                  {partnership.partner ? (
+                    <div>
+                      <h4>Partner Details</h4>
+                      <p>Name: {partnership.partner.name}</p>
+                      <p>EDI Version: {partnership.partner.edi_version}</p>
+                      <p>Delimiters: {partnership.partner.delimiters}</p>
+                      <p>EOL: {partnership.partner.EOL}</p>
+                      <p>Type of Connection: {partnership.partner.type_of_connection}</p>
+                    </div>
+                    ):(
+                      <p>No partner available</p>
+                    )
+                  }
+                </div>
+              ))) : (
               <p>No partnership available</p>
-            )}
+              )
+            }
           </div>
-        ))
+          )
+        )
       ) : (
         <p>No clients found</p>
-      )}
+        )
+      }
     </div>
   );
 };
