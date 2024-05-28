609f3665e7c48a19224ecd5b9e8af4d69c06b456
Said-29 <a01252477@tec.mx>
2024-05-28 14:52:51 -0700

    Updated controllers to catch errors

--- src/da/EDI-Documents/edi-document-da.ts
+++ src/da/EDI-Documents/edi-document-da.ts
@@ -4,74 +4,130 @@ const prisma = new PrismaClient();
 
 //Create EDI-Document
 export async function createEDIdocument(type: string, template: boolean){
-    const EDI_document = prisma.eDI_Document.create({
-        data: {
-            type: type,
-            partner: {},
-            template: template
+    try{
+        const EDI_document = prisma.eDI_Document.create({
+            data: {
+                type: type,
+                partner: {},
+                template: template
+            }
+        });
+        if(!EDI_document){
+            throw new Error("Failed to create EDI_Document");
         }
-    });
-    return EDI_document;
+        return EDI_document;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
 }
 
 //Read all EDI-Documents
 export async function getEDIdocuments(){
-    const EDI_documents = prisma.eDI_Document.findMany();
-    return EDI_documents;
+    try{
+        const EDI_documents = prisma.eDI_Document.findMany();
+        if(!EDI_documents){
+            throw new Error("Failed to fetch EDI_Documents");
+        }
+        return EDI_documents;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
 }
 
 //Read EDI-Document by ID
 export async function getEDIdocumentsById(id: string){
-    const EDI_document = prisma.eDI_Document.findUnique({
-        where: {
-            id: id
+    try {
+        const EDI_document = prisma.eDI_Document.findUnique({
+            where: {
+                id: id
+            }
+        });
+        if(!EDI_document){
+            throw new Error("Failed to fetch EDI_Document");
         }
-    });
-    return EDI_document;
+        return EDI_document;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
 }
 
 //Read EDI-Document by partner_id
 export async function getEDIdocumentsByPartnerId(Partner_id:string){
-    const EDI_documents = prisma.eDI_Document.findMany({
-        where: {
-            partnerId: Partner_id
+    try{
+        const EDI_documents = prisma.eDI_Document.findMany({
+            where: {
+                partnerId: Partner_id
+            }
+        });
+        if(!EDI_documents){
+            throw new Error("Failed to fetch EDI_Documents");
         }
-    });
-    return EDI_documents;
+        return EDI_documents;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
 }
 
 //Update EDI-Document type
 export async function updateEDIdocumentType(id:string, type: string){
-    const EDI_document = prisma.eDI_Document.update({
-        where: {
-            id:id
-        },
-        data: {
-            type: type
+    try{
+        const EDI_document = prisma.eDI_Document.update({
+            where: {
+                id:id
+            },
+            data: {
+                type: type
+            }
+        })
+        if(!EDI_document){
+            throw new Error("Failed to update EDI_Document");
         }
-    })
-    return EDI_document;
+        return EDI_document;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
 }
 
 //Update EDI-document template
 export async function updateEDIdocumentTemplate(id:string, template:boolean) {
-    const EDI_document = prisma.eDI_Document.update({
-        where: {
-            id:id
-        },
-        data: {
-            template: template
+    try{
+        const EDI_document = prisma.eDI_Document.update({
+            where: {
+                id:id
+            },
+            data: {
+                template: template
+            }
+        });
+        if(!EDI_document){
+            throw new Error("Failed to update EDI_Document");
         }
-    });
-    return EDI_document;
+        return EDI_document;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
 }
 
 //Delete EDI-Document by id
 export async function deleteEDIdocument(id: string){
-    const EDI_document = prisma.eDI_Document.delete({
-        where: {
-            id: id
+    try{
+        const EDI_document = prisma.eDI_Document.delete({
+            where: {
+                id: id
+            }
+        });
+        if(!EDI_document){
+            throw new Error("Failed to delete EDI_Document");
         }
-    });
-    return EDI_document;
+        return EDI_document;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
 }
