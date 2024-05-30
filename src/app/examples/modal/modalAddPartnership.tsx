b0c0cd0f52d6ac946fe15a992a2cf61940ec5673
ElMeGaMerX <A01254039@tec.mx>
2024-05-29 19:28:07 -0700

    edit: added Table to display partnerships

--- src/app/examples/modal/modalAddPartnership.tsx
+++ src/app/examples/modal/modalAddPartnership.tsx
@@ -11,31 +11,43 @@ import {
 } from "@/components/ui/dialog";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { DataTable } from "../tables/table/data-table";
+import { columnsModal } from "@/app/client/partnerships/columns";
+import { PartnerShipsClientContent } from "../../../../types/TableTypes";
 
 interface ModalAddPartnerships {
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
   ButtonContent: string;
+  data:PartnerShipsClientContent[];
+  handleIsOpen:()=>void;
 }
 
 export function ModalAddPartnerships({
   isOpen,
   setIsOpen,
   ButtonContent,
+  data,
+  handleIsOpen,
 }: ModalAddPartnerships) {
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <Button variant="default">{ButtonContent} <Plus strokeWidth={1.5} /></Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[625px]">
-        <div className="flex items-center justify-center pt-10">
-          Table
+      <DialogContent className="flex flex-col sm:items-center sm:justify-center sm:max-w-[75%]">
+        <DialogHeader className="flex sm:w-full sm:justify-center sm:items-center sm:text-center sm:text-white font-semibold">
+          <DialogTitle className="">
+            Add Partnership
+          </DialogTitle>
+        </DialogHeader>
+        <div className="flex items-center justify-center pt-5 w-[85%]">
+          <DataTable columns={columnsModal} data={data} />
         </div>
 
         <DialogFooter className="felx items-center justify-center">
           <DialogClose asChild>
-            <Button size="sm" className="h-8 w-[40%]">
+            <Button size="lg" className="h-10">
               Cancel
             </Button>
           </DialogClose>
