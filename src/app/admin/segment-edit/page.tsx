26a2461773b80c817aa0478f83ec9dce73ff803f
ElMeGaMerX <A01254039@tec.mx>
2024-06-04 18:13:10 -0700

    edit: moved segment edit to admin pages

--- src/app/admin/segment-edit/page.tsx
+++ src/app/admin/segment-edit/page.tsx
@@ -6,12 +6,12 @@ import examplejson from "./exmaple-segment.json"
 
 export default function Page() {
   return (
-    <>
+    <div className="flex flex-col w-full justify-center items-center">
       <PageTitle title="Edit Segment" />
 
       <div className="flex w-screen justify-center pb-20">
         <SegmentEdit initialSegmentData={examplejson}/>
       </div>
-    </>
+    </div>
   );
 }
