5f8b091083d1182dc275c20fa3182c8bc0517bbd
Said-29 <a01252477@tec.mx>
2024-06-06 15:02:00 -0700

    modified buttons to redirect to partnership onboarding

--- src/app/client/columns.tsx
+++ src/app/client/columns.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Link from "next/link";
 import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import {
     DropdownMenu,
     DropdownMenuContent,
