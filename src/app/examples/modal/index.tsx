547be0352155383781bd6a501d6af4f465f480d9
m <m@tag.local>
2024-06-11 19:45:27 -0700

    added error fetching

--- src/app/examples/modal/index.tsx
+++ src/app/examples/modal/index.tsx
@@ -112,8 +112,8 @@ export default function Modals({
         <ModalUpload
           isOpen={isOpen}
           setIsOpen={setIsOpen}
-		      partnershipId={partnershipId}
-		      ediType={ediType}
+		  partnershipId={partnershipId}
+		  ediType={ediType}
           ButtonContent="Subir Documento"
         />
       )}
